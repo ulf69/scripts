d85ed18b57b13566f012b4c142d0c5456746d29d
ulf69 <wulf@schuellner.net>
2026-03-05 13:50:52 +0100

    added new gro file manipulations

--- headers/gro_reader.cpp
+++ headers/gro_reader.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <iomanip>
  
 class gro_manipulator 
 {
@@ -38,8 +39,7 @@ std::pair<std::vector<std::string>, std::vector<int>> get_topfile_molecules(){
 	std::vector<std::string>	top_residues;
 	std::vector<int>			top_res_counts;
 		for (int i = lines.size() -1 ; i >= 0; i-- ){			
-				//dh::merge_vecs(top_residues, dh::iss(lines[i])); 
-				
+			
 				std::vector <std::string> vec= dh::iss(lines[i]);
 				top_residues.insert(top_residues.end(), vec.begin(), vec.end());
 
@@ -55,7 +55,7 @@ std::pair<std::vector<std::string>, std::vector<int>> get_topfile_molecules(){
 
 
 void sort_molecules(){
-	auto [top_residues, top_res_counts] =get_topfile_molecules();
+	auto [top_residues, top_res_counts] = get_topfile_molecules();
 
 	//change top.residue to .gro format
 	
@@ -75,17 +75,11 @@ std::vector<std::vector<std::string>> atoms(top_residues.size());
 
 
 	for (int i=2; i <grofile.size()-1 ;i++){
-	//int particle_count;
 	
 		std::string residue_name;
 		residue_name    = grofile[i].substr(5,5);
-	//	particle_count  = std::stoi(grofile[i].substr(15,5)); 
 		for(int j = 0; j<top_residues.size();j++){
 			if(residue_name==top_residues[j]){
-			//	std::cout<<residue_name<<"1"<<'\n';
-			//	std::cout<<top_residues[j]<<"1"<<'\n';
-				//abb::print(residue_name);
-				//abb::print(top_residues[j]);
 				atoms[j].push_back(grofile[i].substr(5,10));
 				coordinates[j].push_back(grofile[i].substr(20));
 			}
@@ -97,8 +91,6 @@ for(int i= 0; i  < atoms.size();i++){
 	abb::print(top_res_counts[i]);
 	residue_size.push_back((atoms[i].size())/top_res_counts[i]);
 }	
-//abb::show(top_residues);
-//abb::show_int(residue_size);
 
 	int residue_count = 1;
 	int atom_count = 0;
@@ -118,63 +110,219 @@ for(int i= 0; i  < atoms.size();i++){
 outfile.close();
 }
 
-};
+std::vector<std::string> get_particles_in_box(double x_min, double x_max,double y_min, double y_max,double z_min, double z_max){
+	std::vector<std::string> particles_in_box;
+	std::string particle_ph;
+	for(int i=2; i < grofile.size() - 1; i++){
+
+		double x_coord = std::stod(grofile[i].substr(20,8));
+		double y_coord = std::stod(grofile[i].substr(28,8));
+		double z_coord = std::stod(grofile[i].substr(36,8));
+		if(x_coord < x_max && x_coord > x_min && y_coord < y_max && y_coord > y_min && z_coord < z_max && z_coord > z_min ){
+			std::string particle = grofile[i].substr(0,5);
+			if(particle == particle_ph){
+				continue;
+			}
+			else{
+				particles_in_box.push_back(particle);
+			}
+			particle_ph = particle;
+		}
+	
+	}
+	
+		return particles_in_box;
+}
+
+void translate_box(std::string box){
+
+	double x_min = 100000000 ;
+	double y_min = 100000000 ;
+	double z_min = 100000000 ;
+	double x_max = 0 ;
+	double y_max = 0 ;
+	double z_max = 0 ;
+
+	std::vector<std::string>	boxfile = dh::whole_file_to_vector(box);
+	
+	int siz = boxfile.size();
+	
+	for(int i = 2; i < siz-1;i++ ){
+		double x_coord = std::stod(boxfile[i].substr(20,8));
+		double y_coord = std::stod(boxfile[i].substr(28,8));
+		double z_coord = std::stod(boxfile[i].substr(36,8));
+		if(x_coord>x_max){
+			x_max = x_coord;
+		}
+		else if(x_coord < x_min){
+			x_min = x_coord;
+		}
+		if(y_coord>y_max){
+			y_max = y_coord;
+		}
+		else if(y_coord < y_min){
+			y_min = y_coord;
+		}
+		if(z_coord > z_max){
+			z_max  = z_coord;
+		}
+		else if(z_coord < z_min){
+			z_min = z_coord;
+		}
+	}
+	std::ofstream outfile("cut_trans_box.gro");
+		outfile<<boxfile[0]<<'\n';
+		outfile<<siz -3<<'\n';
 
 
 	
-////#########################################from here on ################################################
-////#########################################gro file		################################################
-////#########################################manipulation ################################################
-//std::vector<int> residue_size;
-//std::vector<std::vector<std::string>> coordinates(top_residues.size());
-//std::vector<std::vector<std::string>> atoms(top_residues.size());
-//
-//
-//
-//
-//	for (int i=2; i <grofile.size()-1 ;i++){
-//	int particle_count;
-//	
-//	std::string residue_name;
-//		residue_name    = grofile[i].substr(5,5);
-//		particle_count  = std::stoi(grofile[i].substr(15,5)); 
-//		for(int j = 0; j<top_residues.size();j++){
-//			if(residue_name==top_residues[j]){
-//				atoms[j].push_back(grofile[i].substr(5,10));
-//				coordinates[j].push_back(grofile[i].substr(20));
-//			}
-//		}	
-//	}
-//
-//for(int i= 0; i  < atoms.size();i++){
-//	residue_size.push_back((atoms[i].size())/top_res_counts[i]);
-//}	
-//
-//
-//	int residue_count = 1;
-//	int atom_count = 0;
-//	std::ofstream outfile(out_file);
-//	outfile<<grofile[0]<<'\n';
-//	outfile<<grofile[1]<<'\n';
-//for(int i= 0; i  < atoms.size();i++){
-//	for(int j =0 ; j<atoms[i].size();j++){
-//		atom_count++;
-//		outfile<<std::string(5-std::to_string(residue_count).size(),' ')<<residue_count<<atoms[i][j]<<std::string(5-std::to_string(atom_count).size(),' ')<<atom_count<<coordinates[i][j]<<'\n';
-//		if((j+1)%residue_size[i]==0){
-//			residue_count++;
-//		}
-//	}
-//}
-//	outfile<<grofile[grofile.size()-1]<<'\n';
-//outfile.close();
-//}
-//
-//
-//
-//
-//
-//
-//
+	for(int i = 2; i < siz-1;i++ ){
+	
+		double x_coord = std::stod(boxfile[i].substr(20,8)) - x_min;
+		double y_coord = std::stod(boxfile[i].substr(28,8)) - y_min;
+		double z_coord = std::stod(boxfile[i].substr(36,8)) - z_min;
+		
+		
+		outfile<<std::fixed << std::setprecision(3) <<boxfile[i].substr(0,20)<<std::string(11-std::to_string(x_coord).size(),' ')<<x_coord<<std::string(11-std::to_string(y_coord).size(),' ')<<y_coord<<std::string(11-std::to_string(z_coord).size(),' ')<<z_coord<<'\n';	
+	}
+	outfile << "   "<< std::fixed << std::setprecision(5) << x_max-x_min<< "   "<< y_max-y_min<< "   "<< z_max-z_min << "\n";
+	outfile.close();
+
+}
+
+
+void cut_box(double x_min, double x_max,double y_min, double y_max,double z_min, double z_max){
+
+	std::vector<std::string> particles = get_particles_in_box( x_min,  x_max, y_min,  y_max, z_min,  z_max);
+	std::ofstream outfile("cut_box.gro");
+	int j = 0;
+	int count = 0;
+	outfile<<grofile[0]<<'\n';
+	outfile<<particles.size()<<'\n';
+	
+	for(int i = 0; i < grofile.size();i++){
+		if(grofile[i].substr(0,5)==particles[j]){
+		
+			count++;
+			std::string spaces =  std::string(5-std::to_string(j+1).size(),' ');		
+			outfile<<j+1<<spaces<<grofile[i].substr(5,10)<< std::string(5-std::to_string(count).size(),' ') <<count << grofile[i].substr(20,24)<<'\n';			
+		}
+		if(j+1<particles.size() &&grofile[i].substr(0,5)==particles[j+1]){
+			j++;
+			count++;
+				std::string spaces =  std::string(5-std::to_string(j+1).size(),' ');		
+				outfile<<j+1<<spaces<<grofile[i].substr(5,10)<< std::string(5-std::to_string(count).size(),' ') <<count << grofile[i].substr(20,24)<<'\n';			
+		} 		
+	}
+	outfile<<grofile[grofile.size()-1]<<'\n';
+	outfile.close();
+	translate_box("cut_box.gro");
+
+}
+
+void multiply_box(std::string box){
+	std::vector<std::string>	boxfile = dh::whole_file_to_vector(box);
+
+	std::vector<int> x_shift = { 1,  2, 0, 2, 1,  1,  2};
+	std::vector<int> y_shift = { 0,  0, 1, 2, 1,  2,  1};
+	std::ofstream outfile("multiplied_box.gro");
+	outfile<<boxfile[0]<<'\n';
+	outfile<<8*stod(boxfile[1])<<'\n';
+	int siz = boxfile.size();
+
+	for(int i = 2; i < siz-1; i++){
+		outfile	<< boxfile[i]<<'\n';	
+	}
+	int particles_in_starting_box = std::stoi(boxfile[siz-2].substr(0,5));
+	int atom_in_starting_box = std::stoi(boxfile[siz-2].substr(15,5));
+	double box_x = std::stod(boxfile[siz-1].substr(0,10));
+	double box_y = std::stod(boxfile[siz-1].substr(10,10));
+	double box_z = std::stod(boxfile[siz-1].substr(20,10));
+	
+	int step = 0;
+	for(int i = 0; i <x_shift.size();i++ ){
+			step += 1;
+			for(int k = 2; k < siz-1; k++){
+				int particle = step*particles_in_starting_box+std::stoi(boxfile[k].substr(0,5));
+				std::string residue_name = boxfile[k].substr(5,5);
+				std::string atom_name = boxfile[k].substr(10,5);
+				int atom_number = stoi(boxfile[k].substr(15,5))+step*atom_in_starting_box;
+				double x_coord = std::stod(boxfile[k].substr(20,8)) + x_shift[i]*box_x*0.7 ;
+				double y_coord = std::stod(boxfile[k].substr(28,8)) + y_shift[i]*box_y*0.7;
+				double z_coord = std::stod(boxfile[k].substr(36,8));
+
+				
+				outfile<<particle<< std::string(5-std::to_string(particle).size(),' ')<<residue_name<<atom_name<<std::string(5-std::to_string(atom_number).size(),' ')<< atom_number;
+				 
+				outfile<<std::fixed << std::setprecision(3) <<std::string(11-std::to_string(x_coord).size(),' ')<<x_coord<<std::string(11-std::to_string(y_coord).size(),' ')<<y_coord<<std::string(11-std::to_string(z_coord).size(),' ')<<z_coord<<'\n';					
+			}		
+	}
+	
+	outfile << "   " << std::fixed << std::setprecision(5) << box_x+2*box_x*0.7<< "   "<< box_y+box_y*2*0.7<< "   "<< box_z << "\n";
+	
+	outfile.close();
+}
+
+
+
+
+void mirror_box_z(std::string box){
+	std::vector<std::string>	boxfile = dh::whole_file_to_vector(box);
+	std::ofstream outfile("mirrored_box.gro");
+	
+	outfile<<boxfile[0]<<'\n';
+	outfile<<boxfile[1]<<'\n';
+	int siz = boxfile.size();
+	double box_z = std::stod(boxfile[siz-1].substr(24,10));
+	for(int i = 2; i < siz-1; i++){
+		double z_coord = stod(boxfile[i].substr(36,8))*(-1) + box_z ;
+		outfile	<< boxfile[i].substr(0,36)<<std::string(11-std::to_string(z_coord).size(),' ') <<z_coord<<'\n';	
+	}
+    	
+	outfile<<boxfile[siz-1]<<'\n';
+	outfile.close();
+}
+
+
+void merge_boxes(std::string box1, std::string box2){
+	std::vector<std::string>	boxfile1 = dh::whole_file_to_vector(box1);
+	std::vector<std::string>	boxfile2 = dh::whole_file_to_vector(box2);
+
+	std::ofstream outfile("merged_box.gro");		
+	outfile<<boxfile1[0]<<'\n';
+	int all_particles= stoi(boxfile1[1])+stoi(boxfile2[1]);
+	outfile<<all_particles<<'\n';
+
+	int siz1 = boxfile1.size();
+
+	for(int i = 2; i < siz1-1; i++){	
+		outfile	<< boxfile1[i]<<'\n';	
+	}
+	
+	int particles = stoi(boxfile1[siz1-2].substr(0,5));
+	int atoms     = siz1-3;
+	
+	
+	int siz2 = boxfile2.size();
+	abb::print(siz2);
+	for(int i = 2; i < siz2-1; i++){	
+		int particles_box_2 =  stoi(boxfile2[siz2-2].substr(0,5))+particles;
+		int atoms_box_2     =  stoi(boxfile1[siz2-2].substr(15,5))+atoms;
+	//	abb::print(particles_box_2);
+	//	abb::print(atoms_box_2);
+		outfile	<< particles_box_2<<std::string(5-std::to_string(particles_box_2).size(),' ') << boxfile2[i].substr(5,10) <<atoms_box_2<< std::string(5-std::to_string(atoms_box_2).size(),' ') <<boxfile2[i].substr(20)<< '\n';	
+	}
+	double new_box_z = stod(boxfile1[siz1-1].substr(24,7)) + stod(boxfile2[siz2-1].substr(24,7));
+	
+	abb::print(boxfile2[siz1-1].substr(24,7));
+	
+	abb::print(stod(boxfile2[siz1-1].substr(24,7)));
+	
+	outfile<<std::fixed << std::setprecision(5) << boxfile1[siz1-1].substr(0,24)<< new_box_z;
+} 
+};
+
+
 //double overlap_hist(std::string hist1, std::string hist2){
 //
 //	auto [x_hist1,y_hist1] 	  = dh::get_x_y(hist1);
@@ -261,60 +409,6 @@ outfile.close();
 //
 //
 //
-//void function_composition(std::string f_x,std::string g_x){
-//
-//auto [fx,fy] 	  = dh::get_x_y(f_x);
-//auto [gx,gy] 	  = dh::get_x_y(g_x);
-//
-//fx.clear();
-//gx.clear();
-//
-//
-//
-//int vector_size = gy.size();
-//
-//// Create indices 0..n-1
-//std::vector<int> idx(vector_size);
-//for (int i = 0; i< vector_size; i++) idx[i]=i;
-//
-//
-////Sort indices according to gy values
-//std::sort(idx.begin(), idx.end(), [&gy](int i1, int i2){return gy[i1]<gy[i2];});
-//
-//
-//std::vector<double> sorted_gy(vector_size);
-//std::vector<double> permuted_fy(vector_size);
-//
-//for (int i = 0; i < vector_size; ++i) {
-//       sorted_gy[i]   = gy[idx[i]];
-//       permuted_fy[i] = fy[idx[i]];
-//}
-////hier gehts weiter
-//
-//
-//std::vector<double> subvector_gy;
-//std::vector<double> subvector_fy;
-//
-//
-//for (int i = 1; i < vector_size;i++){
-//	if(i==vector_size-1){
-//		std::string filename = "../U_d/U_"+std::to_string(subvector_gy[0])+".xvg";
-//		dh::write_y_x_file( subvector_fy, subvector_gy, filename);	
-//		}
-//	else if (sorted_gy[i] == sorted_gy[i-1]){
-//		subvector_gy.push_back(sorted_gy[i]);
-//		subvector_fy.push_back(permuted_fy[i]);
-//	}	
-//	else{
-//	std::string filename = "../U_d/U_"+std::to_string(sorted_gy[i-1])+".xvg";
-//	dh::write_y_x_file( subvector_fy, subvector_gy, filename);
-//	subvector_fy.clear();
-//	subvector_gy.clear();
-//	
-//	subvector_gy.push_back(sorted_gy[i]);
-//	subvector_fy.push_back(permuted_fy[i]);
-//	}
-//}
-//}
+
 //
 #endif
